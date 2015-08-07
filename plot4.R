088de87e1aa9f4d02486921956a302e7a7482a5a
TobiAdefidipe <adefidipe.oluwatobi@nexencnoocltd.com>
2015-08-07 16:39:40 -0600

    new

--- plot4.R
+++ plot4.R
@@ -31,7 +31,7 @@ plot (Input$Voltage ~ as.POSIXct(paste(Input$Date, Input$Time)), type = "l", xla
 plot (as.POSIXct(paste(Input$Date, Input$Time)),Input$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 points(as.POSIXct(paste(Input$Date, Input$Time)),Input$Sub_metering_2, type = "l", col = "red")
 points(as.POSIXct(paste(Input$Date, Input$Time)),Input$Sub_metering_3, type = "l", col = "blue")
-legend ("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1), col = c("black", "red", "blue"), cex= 0.4)
+legend ("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1), col = c("black", "red", "blue"), cex= 0.5, xjust = 0)
 
 # Fourth Plot
 # 
